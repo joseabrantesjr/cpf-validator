30d1f5ffc40728621697965e33e61186abc78c73
jasonsmitholiveira <jasonsmitholiveira@gmail.com>
2023-09-08 14:25:19 -0300

    update

--- cpf_generator.js
+++ cpf_generator.js
@@ -1,41 +1,37 @@
 function generateValidCpf() {
-  const digits = Array(11).fill().map(() => Math.floor(Math.random() * 10));
-  const cpf = digits.join('');
-
-  // Sanitiza o CPF de entrada
-  cpf = sanitizeInput(cpf);
-
-  // Verifica se o CPF possui exatamente 11 dígitos após a sanitização
-  if (cpf.length !== 11) {
-    return generateValidCpf(); // Gera novamente um CPF até que tenha 11 dígitos
-  }
-
-  // Calcula os dígitos verificadores
-  const j = calculateDigit(() => calculateSum(9));
-  const k = calculateDigit(() => calculateSum(10));
-
-  // Verifica se os dígitos verificadores estão corretos
-  if (j !== parseInt(cpf.charAt(9)) || k !== parseInt(cpf.charAt(10))) {
-    return generateValidCpf(); // Gera novamente um CPF até que os dígitos verificadores estejarem correto
-  }
-
-  return cpf; // Retorna o CPF válido gerado
+    let cpf;
+    do {
+        const digits = Array(9).fill().map(() => Math.floor(Math.random() * 10));
+        digits.push(calculateDigit(() => calculateSum(digits.join(''), 9)));
+        digits.push(calculateDigit(() => calculateSum(digits.join(''), 10)));
+        cpf = digits.join('');
+    } while (cpf.length !== 11);
+    return cpf;
 }
 
-// Função auxiliar para calcular a soma dos dígitos multiplicados pelos pesos
-function calculateSum(multiplier) {
-  let sum = 0;
-  for (let i = 0; i < multiplier; i++) {
-    sum += parseInt(cpf.charAt(i)) * (multiplier + 1 - i);
-  }
-  return sum;
+function calculateSum(cpf, multiplier) {
+    let sum = 0;
+    for (let i = 0; i < multiplier; i++) {
+        sum += parseInt(cpf.charAt(i)) * (multiplier + 1 - i);
+    }
+    return sum;
 }
 
-// Função auxiliar para calcular um dígito verificador
 function calculateDigit(sumFunction) {
-  let sum = sumFunction();
-  let rev = 11 - (sum % 11);
-  return (rev === 10 || rev === 11) ? 0 : rev;
+    let sum = sumFunction();
+    let rev = 11 - (sum % 11);
+    return (rev === 10 || rev === 11) ? 0 : rev;
 }
 
-console.log(generateValidCpf()); // Output: 456.789.012-34
+document.addEventListener('DOMContentLoaded', function () {
+    const generateCPFButton = document.getElementById('generateCPFButton');
+    const cpfInput = document.getElementById('cpfInput');
+    const generatedCPF = document.getElementById('generatedCPF');
+    
+    generateCPFButton.addEventListener('click', function () {
+        const validCPF = generateValidCpf();
+        cpfInput.value = validCPF;
+        checkCPFValidity();
+        generatedCPF.textContent = `CPF gerado: ${validCPF}`;
+    });
+});
